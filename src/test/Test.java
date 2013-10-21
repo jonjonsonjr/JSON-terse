f8334532f4e83f730d3fd85d7ea65b88b6bb50bd
Jon Johnson <jonjonsonjr@gmail.com>
2013-10-21 01:15:04 -0400

    Updated test
    Added an example (using the example JSON response from twitter's 1.1
    API GET statuses/mentions_timeline) to show that using JSON-simple
    can be a bit of a pain to grab just one value from a large JSON
    string.

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.json.terse.JSONArray;
 import org.json.terse.JSONObject;
 import org.json.terse.parser.JSONParser;
 import org.json.terse.parser.ParseException;
@@ -7,6 +8,29 @@ import org.json.terse.parser.ParseException;
 public class Test {
 
 	String json = "{\"foo\":[10.2,1,{\"bar\":\"hello\"}]}";
+	String twitter = 
+		"["
+		+ "{"
+			+ "\"coordinates\": null,"
+			+ "\"favorited\": false,"
+			+ "\"truncated\": false,"
+			+ "\"created_at\": \"Mon Sep 03 13:24:14 +0000 2012\","
+			+ "\"id_str\": \"242613977966850048\","
+			+ "\"entities\": {"
+				+ "\"urls\": [],"
+				+ "\"hashtags\": [],"
+				+ "\"user_mentions\": ["
+					+ "{"
+						+ "\"name\": \"Jason Costa\","
+						+ "\"id_str\": \"14927800\","
+						+ "\"id\": 14927800,"
+						+ "\"indices\": [0,11],"
+						+ "\"screen_name\": \"jasoncosta\""
+					+ "}"
+				+ "]"
+			+ "}"
+		+ "}" +
+		"]";
 	
 	public static void main(String[] args) {
 		try {
@@ -26,5 +50,16 @@ public class Test {
 		System.out.println(obj.arr("foo").dub(0));
 		System.out.println(obj.arr("foo").num(1));
 		System.out.println(obj.arr("foo").dub(1));
+		
+		
+		JSONArray t = (JSONArray) parser.parse(twitter);
+		
+		long a = t.obj(0).obj("entities").arr("user_mentions").obj(0).arr("indices").num(0);
+		
+		System.out.println(a);
+		
+		long b = (Long) ((JSONArray) ((JSONObject) ((JSONArray) ((JSONObject) ((JSONObject) t.get(0)).get("entities")).get("user_mentions")).get(0)).get("indices")).get(0);
+		
+		System.out.println(b);
 	}
 }
